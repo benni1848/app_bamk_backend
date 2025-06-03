d35d056f9cd831b7a37bc72e3ef185379604efde
MarcoEbel <marco-ebel-91@web.de>
2025-06-03 18:10:00 +0200

    changed Movie.js

--- models/Movie.js
+++ models/Movie.js
@@ -4,14 +4,14 @@ const movieSchema = new mongoose.Schema({
   title: { type: String, required: true, unique: true },
   genre: { type: [String], required: true },
   director: { type: String, required: true },
-  producer: { type: String }, // Falls benötigt
+  producer: { type: String },
   duration: { type: Number, required: true },
   releaseDate: { type: Date, required: true },
   rating: { type: Number, min: 0, max: 10 },
   description: { type: String, required: true },
-  cast: { type: [String] }, // Liste von Schauspielern
+  cast: { type: [String] },
   trailerUrl: { type: String },
-  coverImage: { type: String } // URL zum Filmcover
+  coverImage: { type: String }
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
