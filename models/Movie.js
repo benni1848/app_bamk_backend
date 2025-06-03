ebcee6cbc973b90af037776fe6d1d0d334c8bb22
MarcoEbel <marco-ebel-91@web.de>
2025-06-03 18:02:33 +0200

    Removed duplicates of Models corrected Movie.JS

--- models/Movie.js
+++ models/Movie.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
-  id: String,
-  mediatype: String,
-  name: String,
-  genre: [String],
-  director: String,
-  duration: Number,
-  published: Date,
-  imageUrls: [String],
-  trailerUrl: String,
-  description: String,
-  rating: Number, 
+  title: { type: String, required: true, unique: true },
+  genre: { type: [String], required: true },
+  director: { type: String, required: true },
+  producer: { type: String }, // Falls benötigt
+  duration: { type: Number, required: true },
+  releaseDate: { type: Date, required: true },
+  rating: { type: Number, min: 0, max: 10 },
+  description: { type: String, required: true },
+  cast: { type: [String] }, // Liste von Schauspielern
+  trailerUrl: { type: String },
+  coverImage: { type: String } // URL zum Filmcover
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
-
 module.exports = Movie;
