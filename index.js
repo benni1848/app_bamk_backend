62c8d501288fc4b432485e9b7215c124ea6c2c91
En-Ferno <marco-ebel-91@web.de>
2025-06-03 18:23:54 +0200

    changed Index.js
    API-Routen eingefügt etc.

--- index.js
+++ index.js
@@ -3,6 +3,44 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
+<<<<<<< HEAD
+require("dotenv").config();
+
+const movieRoutes = require("./routes/routesMovies"); // Film-Route
+const musicRoutes = require("./routes/routesMusic");  // Musik-Route
+const showRoutes = require("./routes/routesShows");   // Serien-Route
+const gameRoutes = require("./routes/routesGames");   // Spiele-Route
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// MongoDB Atlas Connection
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("MongoDB Atlas verbunden mit der BAMK-Datenbank!"))
+    .catch((err) => console.error("Fehler bei MongoDB-Verbindung:", err.message));
+
+// Check if Server is Runnung Standart-Route
+app.get("/", (req, res) => {
+    res.send("<h1>Server läuft erfolgreich!</h1>");
+});
+
+// API-Routes
+app.use("/movies", movieRoutes);
+app.use("/music", musicRoutes);  
+app.use("/shows", showRoutes);   
+app.use("/games", gameRoutes);   
+
+// Server startt
+app.listen(PORT, () => {
+    console.log(`Server läuft auf http://localhost:${PORT}`);
+=======
 const multer = require("multer");
 const path = require("path");
 const passport = require("passport");
@@ -58,4 +96,5 @@ app.get("/movies/:id", (req, res) => {
 
 app.get("/movies", (req, res) => {
   movieController.getAllMovies(req, res);
+>>>>>>> f5b51edf5dea9380709d150ff1f61ba4c605f2c9
 });
