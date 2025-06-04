6eb25b222c9666fba5cdf04d5c21ce1022bd2dfc
MarcoEbel <marco-ebel-91@web.de>
2025-06-04 16:24:29 +0200

    changes to files/merge cleanup

--- models/Games.js
+++ models/Games.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const gameSchema = new mongoose.Schema({
@@ -15,22 +14,4 @@ const gameSchema = new mongoose.Schema({
 });
 
 const Game = mongoose.model("Game", gameSchema);
-=======
-const mongoose = require("mongoose");
-
-const gameSchema = new mongoose.Schema({
-    title: { type: String, required: true, unique: true },
-    genre: { type: [String], required: true },
-    developer: { type: String, required: true },
-    publisher: { type: String, required: true },
-    platforms: { type: [String], required: true },
-    releaseDate: { type: Date, required: true },
-    rating: { type: Number, min: 0, max: 10 },
-    multiplayer: { type: Boolean, required: true },
-    description: { type: String, required: true },
-    coverImage: { type: String } // URL zum Spielcover
-});
-
-const Game = mongoose.model("Game", gameSchema);
->>>>>>> 99af1c23c3544349fd02dde15a47bc9636b58397
 module.exports = Game;
