a4c48b4c95799938746d31ab79d8dad25c8493cb
MarcoEbel <marco-ebel-91@web.de>
2025-06-20 19:55:55 +0200

    corrected Comment routes
    corrected routes for Comment input for Frontend, comments are now properly stored in DB.
    Disabled Input testing in HTML?!(Why?)

--- routes/routesComments.js
+++ routes/routesComments.js
@@ -33,7 +33,7 @@ router.get("/:username", async (req, res) => {
 // Kommentare lesen (Spiele)
 router.get("/games", async (req, res) => {
     try {
-        const comments = await Comment.find({ mediatype: 1 });
+        const comments = await Comment.find({ mediatype: "1" });
         res.status(200).json(comments);
     } catch (error) {
         console.error("Fehler beim Abrufen der Kommentare:", error.message);
@@ -41,17 +41,15 @@ router.get("/games", async (req, res) => {
     }
 });
 
-// Test-Form für Input-Testing
+/*// Test-Form für Input-Testing
 router.get('/games/comment', (req, res) => {
     res.render('formgame');
-});
+});*/
 
 // Kommentar posten (Spiel)
-router.post("/games/comment/post", async (req,res) => {
+router.post("/games", async (req,res) => {
     try {
-        const { title, inhalt, id } = req.body;
-
-        const username = "Baker"; // festgelegter Nutzername
+        const { title, inhalt, id, username } = req.body;
         const mediatype = "1";    // festgelegter Medientyp
 
         const aktualisierterKommentar = await Comment.findOneAndUpdate(
@@ -69,12 +67,13 @@ router.post("/games/comment/post", async (req,res) => {
         console.error("Fehler beim Posten des Kommentars:", error.message);
         res.status(500).json({ message: "Interner Serverfehler" });
     }
+    console.log("POST eingegangen:", req.body);
 });
 
 // Kommentare lesen (Filme)
 router.get("/movies", async (req, res) => {
     try {
-        const comments = await Comment.find({ mediatype: 2 });
+        const comments = await Comment.find({ mediatype: "2" });
         res.status(200).json(comments);
     } catch (error) {
         console.error("Fehler beim Abrufen der Kommentare:", error.message);
@@ -82,17 +81,15 @@ router.get("/movies", async (req, res) => {
     }
 });
 
-// Test-Form für Input-Testing
+/*// Test-Form für Input-Testing
 router.get('/movies/comment', (req, res) => {
     res.render('formmovie');
-});
+});*/
 
 // Kommentar posten (Film)
-router.post("/movies/comment/post", async (req,res) => {
+router.post("/movies", async (req,res) => {
     try {
-        const { title, inhalt, id } = req.body;
-
-        const username = "Baker"; // festgelegter Nutzername
+        const { title, inhalt, id, username } = req.body;
         const mediatype = "2";    // festgelegter Medientyp
 
         const aktualisierterKommentar = await Comment.findOneAndUpdate(
@@ -110,12 +107,13 @@ router.post("/movies/comment/post", async (req,res) => {
         console.error("Fehler beim Posten des Kommentars:", error.message);
         res.status(500).json({ message: "Interner Serverfehler" });
     }
+    console.log("POST eingegangen:", req.body);
 });
 
 // Kommentare lesen (Musik)
 router.get("/music", async (req, res) => {
     try {
-        const comments = await Comment.find({ mediatype: 3 });
+        const comments = await Comment.find({ mediatype: "3" });
         res.status(200).json(comments);
     } catch (error) {
         console.error("Fehler beim Abrufen der Kommentare:", error.message);
@@ -129,11 +127,9 @@ router.get('/music/comment', (req, res) => {
 });
 
 // Kommentar posten (Musik)
-router.post("/music/comment/post", async (req,res) => {
+router.post("/music", async (req,res) => {
     try {
-        const { title, inhalt, id } = req.body;
-
-        const username = "Baker"; // festgelegter Nutzername
+        const { title, inhalt, id, username } = req.body;
         const mediatype = "3";    // festgelegter Medientyp
 
         const aktualisierterKommentar = await Comment.findOneAndUpdate(
@@ -151,12 +147,13 @@ router.post("/music/comment/post", async (req,res) => {
         console.error("Fehler beim Posten des Kommentars:", error.message);
         res.status(500).json({ message: "Interner Serverfehler" });
     }
+    console.log("POST eingegangen:", req.body);
 });
 
 // Kommentare lesen (Serien)
 router.get("/shows", async (req, res) => {
     try {
-        const comments = await Comment.find({ mediatype: 4 });
+        const comments = await Comment.find({ mediatype: "4" });
         res.status(200).json(comments);
     } catch (error) {
         console.error("Fehler beim Abrufen der Kommentare:", error.message);
@@ -164,17 +161,15 @@ router.get("/shows", async (req, res) => {
     }
 });
 
-// Test-Form für Input-Testing
+/*// Test-Form für Input-Testing
 router.get('/shows/comment', (req, res) => {
     res.render('formshow');
-});
+});*/
 
 // Kommentar posten (Serie)
-router.post("/shows/comment/post", async (req,res) => {
+router.post("/shows", async (req,res) => {
     try {
-        const { title, inhalt, id } = req.body;
-
-        const username = "Baker"; // festgelegter Nutzername
+        const { title, inhalt, id, username } = req.body;
         const mediatype = "4";    // festgelegter Medientyp
 
         const aktualisierterKommentar = await Comment.findOneAndUpdate(
